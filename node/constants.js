77d84e18192acb9369ce1416f826ca4dba3f9b80
Niño James A. Tan <njtan142@gmail.com>
2023-10-15 22:29:04 +0800

    generated samples and json dataset

--- node/constants.js
+++ node/constants.js
@@ -5,6 +5,7 @@ constants.RAW_DIRECTORY = constants.DATA_DIRECTORY + "/raw"
 constants.DATASET_DIRECTORY = constants.DATA_DIRECTORY + "/dataset"
 constants.JSON_DIRECTORY = constants.DATASET_DIRECTORY + "/json"
 constants.IMAGES_DIRECTORY = constants.DATASET_DIRECTORY + "/images"
+constants.SAMPLES_DIRECTORY = constants.DATASET_DIRECTORY + "/samples.json"
 
 
 
